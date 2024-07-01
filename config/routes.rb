7c9b72a88b6caf23df4ccd437b63dda61cc99621
Dimitri Almon <dimitri.almon@gmail.com>
2024-07-01 10:58:09 +0200

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Define the route for asking a question
+  get '/ask', to: 'questions#ask', as: :ask
+
+  # Define the route for answering a question
+  get '/answer', to: 'questions#answer', as: :answer
+
   # Defines the root path route ("/")
+  # You can uncomment and set the root path if needed
   # root "posts#index"
 end
